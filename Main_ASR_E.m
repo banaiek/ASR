8b3a1227c26a9199ad0a2845e6d94399771221bc
Kianoush Banaie Boroujeni <Kianoush.banaie.boroujeni@vanderbilt.edu>
2021-08-03 15:52:23 -0500

    Add files via upload

--- Main_ASR_E.m
+++ Main_ASR_E.m
@@ -1,7 +1,7 @@
 %% Adaptive Spike Removal method
 %% this method is supposed to remove spike-related transient from Wideband data
 %The example data set should be load directly which is a struct named in
-%in.trial_time: shows the time series for time of each trial 
+%in.trial_time: shows the time series for time of each trial
 %in.Spike_trial is the trial number that spikes were recorded
 %in.Spike_cell is the corresponded to the trough of the spike (indicies of
 %spikes which their time is in another parameter named in.Spike_time)
@@ -26,31 +26,34 @@
 
 %%----------
 %written by Kia. Banaie Boroujeni 2018
-clc
+% clc
 close all
 clear all
 
 load('Dataset_example.mat') %loding the dataset example
 
- hp.Fs=32000; %sampling rate Hz
- hp.a1ms=400; %time (msec.) of extraction after the spike trough
- hp.b1ms=400; %time (msec.) of extraction before the spike trough
- hp.Fpl=2;%lower limit for pspectrum to find the minimum frequency of decomposition
- hp.Fpu=200;%upper limit for pspectrum to find the minimum frequency of decomposition
- hp.MAIa= 5; %miss-alignment time interval (msec.) after the current spike indices
- hp.MAIb= 5; %miss-alignment time interval (msec.) before the current spike indices 
- hp.l_cutoff=2; %setting a cut off frequency for removing low freq. decomposition depending on how noisy the data is
- %% Spike trough alignment Function
- [hp,in]=Spike_align(hp,in);
- %% Spike removal Function
-tic;  out=ASR_Func(hp,in); toc;    
-    
+hp.Fs=32000; %sampling rate Hz
+hp.a1ms=400; %time (msec.) of extraction after the spike trough
+hp.b1ms=400; %time (msec.) of extraction before the spike trough
+hp.StDur=500; %time (msec.) of spike triggered average
+hp.Fpl=2;%lower limit for pspectrum to find the minimum frequency of decomposition
+hp.Fpu=200;%upper limit for pspectrum to find the minimum frequency of decomposition
+hp.MAIa= 5; %miss-alignment time interval (msec.) after the current spike indices
+hp.MAIb= 5; %miss-alignment time interval (msec.) before the current spike indices
+hp.l_cutoff=2; %setting a cut off frequency for removing low freq. decomposition depending on how noisy the data is
+%% Spike trough alignment Function
+[hp,in]=Spike_align(hp,in);
+%% Spike removal Function
+tic;  out=ASR_Func(hp,in); toc;
+
 
 %% Plotting out.stLFP for raw and ASR
-      figure,
-          plot([(1:length(out.stLFP_raw))/(hp.Fs/1000)-find(out.stLFP_raw==min(out.stLFP_raw))/(hp.Fs/1000)],out.stLFP_ASR,'r','LineWidth',1)
-          hold on;
-          plot([(1:length(out.stLFP_raw))/(hp.Fs/1000)-find(out.stLFP_raw==min(out.stLFP_raw))/(hp.Fs/1000)],out.stLFP_raw,'b','LineWidth',1)
+figure,
+t=[(1:length(out.stLFP_raw))/(hp.Fs/1000)-find(out.stLFP_raw==min(out.stLFP_raw))/(hp.Fs/1000)];
 
-          xlabel({'Time (mS)'});
-          ylabel({'Amplitude'});
+plot(t,out.stLFP_ASR,'r','LineWidth',1)
+hold on;
+plot(t,out.stLFP_raw,'b','LineWidth',1)
+
+xlabel({'Time (mS)'});
+ylabel({'Amplitude'});
